f624085e7dbdc9f007358cdc913f16638d2229b7
letsgo2py <118923690+letsgo2py@users.noreply.github.com>
2024-09-26 00:03:25 +0530

    Update ISBN.jsx

--- src/components/ISBN.jsx
+++ src/components/ISBN.jsx
@@ -27,7 +27,7 @@ function ISBN(props) {
 
             if (response.ok) {
                 console.log('Book added successfully');
-                navigate('https://bookish-worm-frontend.onrender.com'); // Redirect to home page
+                navigate('/'); // Redirect to home page
             } else {
                 console.error('Failed to add the book');
             }
