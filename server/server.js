7a534a52a557bdcc02e83cad27e359d3021def50
Abhay <abhayrajtt0448@gmail.com>
2024-09-28 16:39:27 +0530

    solving the cors error

--- server/server.js
+++ server/server.js
@@ -11,10 +11,17 @@ const port = process.env.PORT || 3000;
 // app.use(cors()); 
 
 // Or, specify allowed origins
+const allowedOrigins = ['https://bookish-worm-frontend.onrender.com'];
+
 app.use(cors({
-    // origin: 'http://localhost:5173' // Allow only from this origin
-    origin: 'https://bookish-worm-frontend.onrender.com',// Allow only from this origin
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed methods
+    origin: function (origin, callback) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
 }));
 
